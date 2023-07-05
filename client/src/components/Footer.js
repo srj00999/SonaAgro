ebeeba227543cf4f9be542dfc4056a0ad5242db0
srj00999 <srj00999@gmail.com>
2023-07-05 22:12:23 +0530

    footer update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,16 +6,17 @@ const Footer = () => {
       <div className="footer-text-heading">
         <h1>Contact</h1>
         <h4>Sona Agro</h4>
-        <h4>114/16 Kharyani Road</h4>
-        <h4>Azamgarh Pin 276001</h4>
+        <h4>Belhadih- Bahorikpur Road</h4>
+        <h4> Belhadih Kharihani</h4>
+        <h4>Azamgarh UP 276123</h4>
       </div>
       <div className="footer-text">
-        <h4>Mobile.7317000499</h4>
-        <h4>Email:rohansingh8388@gmail.com</h4>
+        <h4>Mobile.7399000499</h4>
+        <h4>Email:sonaagroazm@gmail.com</h4>
         <h4>Book a Consulation</h4>
       </div>
       <div className="footer-text">
-        <h4>Subscribe to my Newsletter</h4>
+        <h4>Follow Us</h4>
       </div>
       <div className="footer-text">
         <h4>Subscribe to my Newsletter</h4>
