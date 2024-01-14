02b091904003379f96803fc26b8275bc533b30df
Suraj Yadav <srj00999@gmail.com>
2024-01-14 10:07:39 +0530

    update

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB).then(() => {
+
+mongoose.connect("mongodb+srv://srj00999:PTevoDKTeaGLX3J7@users.hjgncvi.mongodb.net/FishFarm?retryWrites=true&w=majority").then(() => {
     console.log("DB connection successful");
   })
   .catch((err) => {
-    console.log(`Not connected DB ${err} `);
+    console.log(`Error not connected DB ${err} `);
   });
