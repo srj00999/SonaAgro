6b3379ac3f1831b6e4f0879e7228e9a13eda34bd
Suraj Yadav <srj00999@gmail.com>
2024-01-15 10:19:09 +0530

    Loader update

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import About from './Components/About';
 import Service from './Components/Service';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+import Loader from './Components/Loader'; // Import the Loader component
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => {
+      // Clear timeout if component unmounts before loading completes
+      clearTimeout(loadingTimeout);
+    };
+  }, []);
+
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      {loading ? (
+        // Render Loader component while loading
+        <Loader />
+      ) : (
+        // Render content after loading
+        <Router>
+          <div className="App">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Service />} />
+              <Route path="/Contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      )}
+    </>
   );
 }
 
---
+++ client/src/components/Loader.js
@@ -0,0 +1,13 @@
+// Loader.js
+import React from 'react';
+import '../assets/Styles/Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-overlay">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
+export default Loader;
